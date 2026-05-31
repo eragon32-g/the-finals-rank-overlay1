13ab79086e8d216ea751cb61c5245773453f88d0
Sabrina V2 <dusan32peska@icloud.com>
2026-05-31 22:02:19 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# RankTag BETA 0.4.0
+# RankTag BETA 0.4.2
 
 Nickname Only Account Privacy Fix.
+
+
+## BETA 0.4.2
+- Fix scroll pagina Account/Login & Progetti su schermi piccoli.
+- Sincronizza tutte le etichette versione visibili rimaste a 0.4.0.
+- Ripristina voce Updates per 0.4.2.
--- api/version.js
+++ api/version.js
@@ -1,7 +1,7 @@
 module.exports = function handler(req, res) {
   res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
   res.status(200).json({
-    version: "22",
+    version: "0.4.2",
     expectedBrand: "ERDRAGON32",
     expectedDiscord: "discord.gg/cffTwCcCGD",
     note: "If /api does not work, use /version.json and /brand.json."
