c32e640a297890babccbbb64279bb1987d59c49e
Sabrina V2 <dusan32peska@icloud.com>
2026-06-02 13:20:11 +0200

    Add files via upload

--- api/version.js
+++ api/version.js
@@ -1,7 +1,8 @@
 module.exports = function handler(req, res) {
   res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
   res.status(200).json({
-    version: "0.4.2",
+    version: "0.5.2",
+    name: "Feedback & Report Center",
     expectedBrand: "ERDRAGON32",
     expectedDiscord: "discord.gg/cffTwCcCGD",
     note: "If /api does not work, use /version.json and /brand.json."
